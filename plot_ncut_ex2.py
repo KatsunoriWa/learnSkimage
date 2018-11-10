735511930fd474d3e822ad28e570142e06ee15c5
papa <gtx285aa@yahoo.co.jp>
2018-11-10 22:42:38 +0900

    added __name__

--- plot_ncut_ex2.py
+++ plot_ncut_ex2.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
-from skimage import data, io, segmentation, color
+from skimage import io, segmentation, color
 from skimage.future import graph
-from matplotlib import pyplot as plt
 
 def plotNcut(img):
-    labels1 = segmentation.slic(img, compactness=30, n_segments=400)
-    out1 = color.label2rgb(labels1, img, kind='avg')
+    """
+    img: color img
 
+    """
+
+    labels1 = segmentation.slic(img, compactness=30, n_segments=400)
     g = graph.rag_mean_color(img, labels1, mode='similarity')
     labels2 = graph.cut_normalized(labels1, g)
-    out2 = color.label2rgb(labels2, img, kind='avg')
-
-    plt.figure(1)
-    io.imshow(out1)
-    plt.figure(2)
-    io.imshow(out2)
-    io.show()
+    return color.label2rgb(labels2, img, kind='avg'), labels2
 
 if __name__ == "__main__":
-    import cv2
     import glob
     for name in glob.glob("*/*.png"):
-        img = cv2.imread(name)[:, :, ::-1]
-        plotNcut(img)
-    #    cv2.waitKey(100)
-
+        img = io.imread(name)
+        out, _ = plotNcut(img)
+        io.imshow(out)
+        io.show()
